20b2624481f002e7da05adf90119349ad36d6c4f
hoangtuan <trian672008@gmail.com>
2026-02-03 15:23:37 +0700

    do an beta

--- includes/footer.php
+++
@@ -1,6 +0,0 @@
-</main>
-<footer>
-    © 2026 - Đồ án PHP | Bán vé xem phim
-</footer>
-</body>
-</html>
--- ketnoi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "movie_ticket");
-mysqli_set_charset($conn, "utf8");
-
-if (!$conn) {
-    die("Kết nối CSDL thất bại");
-}
-?>
